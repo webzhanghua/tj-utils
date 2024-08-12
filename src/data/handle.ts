15ee2508db2f15c2cb537c7f671daeccf39b6133
张华 <15901122405@163.com>
2024-08-12 17:54:59 +0800

    feat: 统计行内数据连续重复次数方法

--- src/data/handle.ts
+++ src/data/handle.ts
@@ -122,3 +122,103 @@ export function optionsHandle(data: any, config: OptionsConfigType = {}) {
     }
   }
 }
+/**
+ * 规范化唯一性判断的键数组
+ *
+ * @param key 唯一性判断的键
+ * @returns 规范化后的键数组
+ */
+function normalizeUniqueKeys(key: string | string[]): string[] {
+  return Array.isArray(key) ? key : [key]
+}
+
+/**
+ * 处理数组元素
+ *
+ * @param updatedArr 更新后的数组
+ * @param totalObjects totalObjects 对象
+ * @param index 当前元素的索引
+ * @param keys 用于比较重复的键数组
+ * @param normalizedUniqueKeys 规范化后的唯一性判断的键数组
+ */
+function processArrayElements(
+  updatedArr: any[],
+  totalObjects: Record<string, Map<any, number>>,
+  i: number,
+  keys: string[],
+  normalizedUniqueKeys: string[],
+  length: number
+): void {
+  const current = updatedArr[i]
+  const previous = updatedArr[i - 1]
+  for (let j = 0; j < keys.length; j++) {
+    const key = keys[j]
+    // 前一项数据
+    const preValue = previous?.[key]
+    // 当前项数据
+    const value = current?.[key]
+
+    if (i === length) {
+      totalObjects[key] = new Map([[value, 1]])
+    }
+    let isDifferentGroup = false
+    if (
+      Array.isArray(normalizedUniqueKeys) &&
+      normalizedUniqueKeys.length > 0
+    ) {
+      isDifferentGroup = normalizedUniqueKeys.some(key => {
+        return previous?.[key] !== current?.[key]
+      })
+    }
+    if (isDifferentGroup || preValue !== value) {
+      // 不属于同一组数据 或  属于同一组数据，但前后数据不同
+      updatedArr[i][key + '_total'] = totalObjects?.[key]?.get(value) || 1
+      // 设置缓存中前一项初始数据
+      totalObjects[key] = new Map([[preValue, 1]])
+    } else {
+      // 属于同一组数据，前后数据相同
+      const newTotal = (totalObjects?.[key]?.get(preValue) || 0) + 1
+      // 更新缓存中当前项数据
+      totalObjects[key].set(preValue, newTotal)
+      // 设置当前项数据
+      updatedArr[i][key + '_total'] = 0
+    }
+    if (i === 1) {
+      // 设置第一项数据
+      updatedArr[0][key + '_total'] = totalObjects?.[key]?.get(value)
+    }
+  }
+}
+
+/**
+ * 统计行内数据连续重复次数，以 [key]_total 字段存储, 重复项开始保存总数，其它项保存为0
+ *
+ * @param arr 待处理的数组
+ * @param keys 重复行判断的关键字数组
+ * @param uniqueKeys 唯一标识的关键字数组或单个关键字
+ * @returns 处理后的数组
+ */
+export const repeatedRowCalculationHandle = (
+  arr: any[],
+  keys: string[],
+  uniqueKeys: string | string[]
+): any[] => {
+  if (arr.length < 2 || keys.length === 0) return arr
+
+  const normalizedUniqueKeys = normalizeUniqueKeys(uniqueKeys)
+  const updatedArr = [...arr]
+  const totalObjects: Record<string, Map<any, number>> = {}
+  const length = updatedArr.length - 1
+
+  for (let i = length; i > 0; i--) {
+    processArrayElements(
+      updatedArr,
+      totalObjects,
+      i,
+      keys,
+      normalizedUniqueKeys,
+      length
+    )
+  }
+  return updatedArr
+}
